3653706f2543990712f158ae1d40b374c2899eb8
Vinicius Santana <santanaviny@gmail.com>
2018-12-13 00:11:01 -0800

    Updates

---
+++ precache-manifest.54e8ac908432e5fadeda9d0ac981ace3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e18139124400b5999e6a",
+    "url": "/react-d3/static/css/main.578e4977.chunk.css"
+  },
+  {
+    "revision": "e18139124400b5999e6a",
+    "url": "/react-d3/static/js/main.e1813912.chunk.js"
+  },
+  {
+    "revision": "f9670b970e906fa05933",
+    "url": "/react-d3/static/js/1.f9670b97.chunk.js"
+  },
+  {
+    "revision": "fc4de8f5848ca7f819e3",
+    "url": "/react-d3/static/js/runtime~main.fc4de8f5.js"
+  },
+  {
+    "revision": "f1d8f2e94e63a4528d4f6b4aabf695fc",
+    "url": "/react-d3/index.html"
+  }
+];
